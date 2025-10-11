508d03900d459ce3bacd54f93c8a41c2a0ab8cdc
jonathansvaerke <jonathansvaerke@proton.me>
2025-10-11 11:50:02 +0200

    Create a config for eduroam certificate

--- hosts/common/optional/default.nix
+++ hosts/common/optional/default.nix
@@ -1,6 +1,7 @@
 {
   imports = [
     #./direnv.nix
+    ../../../modules/nixos/eduroam.nix
     ./fonts.nix
     ./gdm.nix
     ./gnome.nix
---
+++ modules/nixos/eduroam.nix
@@ -0,0 +1,40 @@
+{ config, ... }:
+{
+  sops.templates."eduroam-certificate" = {
+    content = ''
+      [connection]
+      id=eduroam
+      uuid=906422a9-468b-4505-8a6d-b42fffcd1141
+      type=wifi
+      interface-name=wlp2s0
+
+      [wifi]
+      mode=infrastructure
+      ssid=eduroam
+
+      [wifi-security]
+      auth-alg=open
+      key-mgmt=wpa-eap
+
+      [802-1x]
+      eap=peap;
+      identity=${config.sops.placeholder."at/username"}
+      password=${config.sops.placeholder."at/password"}
+      phase2-auth=mschapv2
+
+      [ipv4]
+      method=auto
+
+      [ipv6]
+      addr-gen-mode=default
+      method=auto
+
+      [proxy]
+    '';
+  };
+
+  environment.etc."NetworkManager/system-connections/eduroam.nmconnection" = {
+    source = config.sops."eduroam-certificate".path;
+    mode = "0600";
+  };
+}
