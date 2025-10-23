ad9a813e1a54b7b2dd8b1c0c0cdad47edea6d19c
jonathansvaerke <jonathansvaerke@proton.me>
2025-10-23 17:29:17 +0200

    Add initial config for nvf

--- flake.nix
+++ flake.nix
@@ -18,6 +18,11 @@
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
--- home/jonathan/editors/neovim/default.nix
+++ home/jonathan/editors/neovim/default.nix
@@ -1,7 +1,67 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
-  programs.neovim = {
+  imports = [ inputs.nvf.homeManagerModules.default ];
+  programs.nvf = {
     enable = true;
-    package = pkgs.neovim-unwrapped;
+    settings = {
+      vim = {
+        languages = {
+          nix = {
+            enable = true;
+            format = {
+              enable = true;
+              package = pkgs.nixfmt-rfc-style;
+              type = "nixfmt";
+            };
+            lsp = {
+              enable = true;
+              #options = {};
+              package = pkgs.nixd;
+              server = "nixd";
+            };
+            treesitter = {
+              enable = true;
+            };
+          };
+          python = {
+            enable = true;
+            dap = {
+              enable = true;
+
+            };
+            format = {
+              enable = true;
+              package = pkgs.ruff;
+              type = "ruff";
+            };
+            lsp = {
+              enable = true;
+            };
+            treesitter = {
+              enable = true;
+            };
+          };
+          zig = {
+            enable = true;
+            dap = {
+              enable = true;
+            };
+            lsp = {
+              enable = true;
+              package = pkgs.zls;
+              server = "zls";
+            };
+            treesitter = {
+              enable = true;
+            };
+          };
+        };
+        theme = {
+          enable = true;
+          name = "tokyonight";
+          style = "dark";
+        };
+      };
+    };
   };
 }
