36348d27f61bb7709426e11ca34bf6402507d265
jonathansvaerke <jonathansvaerke@proton.me>
2025-10-29 19:42:41 +0100

    Fix nvf config

--- home/jonathan/editors/neovim/default.nix
+++ home/jonathan/editors/neovim/default.nix
@@ -3,65 +3,6 @@
   imports = [ inputs.nvf.homeManagerModules.default ];
   programs.nvf = {
     enable = true;
-    settings = {
-      vim = {
-        languages = {
-          nix = {
-            enable = true;
-            format = {
-              enable = true;
-              package = pkgs.nixfmt-rfc-style;
-              type = "nixfmt";
-            };
-            lsp = {
-              enable = true;
-              #options = {};
-              package = pkgs.nixd;
-              server = "nixd";
-            };
-            treesitter = {
-              enable = true;
-            };
-          };
-          python = {
-            enable = true;
-            dap = {
-              enable = true;
-
-            };
-            format = {
-              enable = true;
-              package = pkgs.ruff;
-              type = "ruff";
-            };
-            lsp = {
-              enable = true;
-            };
-            treesitter = {
-              enable = true;
-            };
-          };
-          zig = {
-            enable = true;
-            dap = {
-              enable = true;
-            };
-            lsp = {
-              enable = true;
-              package = pkgs.zls;
-              server = "zls";
-            };
-            treesitter = {
-              enable = true;
-            };
-          };
-        };
-        theme = {
-          enable = true;
-          name = "tokyonight";
-          style = "dark";
-        };
-      };
-    };
+    settings.vim = import ./nvf.nix { inherit pkgs; };
   };
 }
