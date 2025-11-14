9cb49804230e79214ed0f3caa4f5bf85e52a5d85
jonathansvaerke <jonathansvaerke@proton.me>
2025-11-14 20:22:06 +0100

    Enable nvidia for docker, only if the host uses nvidia

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,6 +1,9 @@
+{ config, lib, ... }:
 {
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
   };
+
+  hardware.nvidia-container-toolkit.enable = lib.mkIf config.hardware.nvidia.open true;
 }
