68aa93494a84da34e46b422834df725745c0f04c
jonathansvaerke <jonathansvaerke@proton.me>
2025-11-15 20:59:31 +0100

    Fix(nvidia bug): Fix for non-nvidia hosts

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -5,5 +5,5 @@
     enableOnBoot = true;
   };
 
-  hardware.nvidia-container-toolkit.enable = lib.mkIf config.hardware.nvidia.open true;
+  hardware.nvidia-container-toolkit.enable = lib.mkIf (config.hardware.nvidia.open or false) true; # Check the value, if defined take that, but if the option isn't defined, set it as false.
 }
