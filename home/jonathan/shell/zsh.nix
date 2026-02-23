f2c935faf7d2a908f52dbe81fb35ac34f79313b8
jonathansvaerke <jonathansvaerke@proton.me>
2026-02-23 14:14:35 +0100

    Quick save

--- home/jonathan/shell/zsh.nix
+++ home/jonathan/shell/zsh.nix
@@ -3,16 +3,25 @@
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+
     syntaxHighlighting = {
       enable = true;
       highlighters = [ "main" ]; # Default highlighter
     };
+
+    initContent = ''
+      PROMPT='%n@%m > '
+    ''; # user@host: path/to/directory/ > $  =>  user@host > $
+
     shellAliases = {
+      p = "pwd";
+      pl = " pwd && ls";
       clr = "clear";
-      ncg = "sudo nix-collect-garbage -d";
+
       po = "poweroff";
       sdn = "shutdown now";
 
+      ncg = "sudo nix-collect-garbage -d";
       pico8 = "steam-run /home/jonathan/Desktop/pico-8/pico8";
     };
   };
